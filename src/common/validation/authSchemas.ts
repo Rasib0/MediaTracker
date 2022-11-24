8d1813711c5c46ca701728faa049cfa53a5b1810
Rasib <rasibnadeem101@gmail.com>
2022-11-24 17:30:55 +0500

    renamed the searchSchema schema

--- src/common/validation/authSchemas.ts
+++ src/common/validation/authSchemas.ts
@@ -9,5 +9,12 @@ export const signUpSchema = loginSchema.extend({
   username: z.string(),
 });
 
+export const searchSchema = z.object({
+  keywords: z.string().max(100),
+  tags: z.string().max(20).array(),
+});
+
+
 export type ILogin = z.infer<typeof loginSchema>;
 export type ISignUp = z.infer<typeof signUpSchema>;
+
