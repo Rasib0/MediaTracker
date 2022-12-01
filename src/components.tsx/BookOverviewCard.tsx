0143c8015484a809a00bf9325ccd2480dee35f4b
Rasib <rasibnadeem101@gmail.com>
2022-12-02 00:45:12 +0500

    em tag for author name

--- src/components.tsx/BookOverviewCard.tsx
+++ src/components.tsx/BookOverviewCard.tsx
@@ -24,7 +24,7 @@ const BookOverviewCard: React.FC<CardProps> = (props: CardProps) => {
         <div className="col-md-8">
           <div className="card-body">
             <h5 className="card-title">{props.name} </h5>
-            <div className="card-text">by {props.by} </div>
+            <div className="card-text"><em>by  {props.by}</em> </div>
             <p className="card-text">{props.synopsis.substring(0, 150)}...</p>
           </div>
         </div>
