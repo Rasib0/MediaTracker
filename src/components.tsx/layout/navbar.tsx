69e52bd6d062dbdeaf59e64a015bce95cb8c574b
Rasib0 <rasibnadeem101@gmail.com>
2023-06-19 03:48:14 +0500

    changed to next Image

--- src/components.tsx/layout/navbar.tsx
+++ src/components.tsx/layout/navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { currentPage } from "../../common/types";
+import Image from "next/image";
 
 type Props = {
   currentPage: currentPage;
@@ -51,15 +52,11 @@ const Navbar = (props: Props) => {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
             <div className="flex flex-shrink-0 items-center">
-              <img
-                className="block h-8 w-auto lg:hidden"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              <Image
+                src="/images/logo.svg"
                 alt="Your Company"
-              />
-              <img
-                className="hidden h-8 w-auto lg:block"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
+                width={40}
+                height={40}
               />
             </div>
             <div className="hidden sm:ml-6 sm:block">
