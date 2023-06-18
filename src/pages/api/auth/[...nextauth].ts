6629c9afbe2a0adaac7a78d81a93ed1ac37b8dc9
Rasib0 <rasibnadeem101@gmail.com>
2023-06-19 00:46:38 +0500

    testing with any

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,65 @@
 import NextAuth from "next-auth";
+import { NextAuthOptions } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { verify } from "argon2";
+import { prisma } from "../../../server/prisma";
+import { loginSchema } from "~/common/validation/authSchemas";
 
-import { nextAuthOptions } from "../../../common/auth";
+export const nextAuthOptions: NextAuthOptions = {
+    providers: [
+        Credentials({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "jsmith@gmail.com" },
+                password: { label: "Password", type: "password" },
+            },
+
+            authorize: async (credentials): Promise<any> => {
+                try {
+                    const { email, password } = await loginSchema.parseAsync(credentials);
+                    const resultUser = await prisma.user.findFirst({
+                        where: { email },
+                    });
+                    if (!resultUser) return null;
+
+                    const isValidPassword = await verify(resultUser.password, password);
+                    if (!isValidPassword) return null;
+                    return { id: resultUser.id, email, username: resultUser.username };
+                } catch {
+                    return null;
+                }
+            },
+        }),
+
+    ],
+    callbacks: {
+        jwt: async ({ token, user }) => {
+            if (user) {
+                token.userId = user.id;
+                token.email = user.email;
+                token.username = user.username;
+            }
+
+            return token;
+        },
+        session: async ({ session, token }) => {
+            if (token) {
+                session.user.userId = token.userId;
+                session.user.email = token.email;
+                session.user.username = token.username;
+            }
+
+            return session;
+        },
+    },
+    jwt: {
+        maxAge: 15 * 24 * 30 * 60, // 15 days
+    },
+    pages: {
+        signIn: "/sign-in",
+        newUser: "/sign-up",
+    },
+    secret: "super-secret",
+};
 
 export default NextAuth(nextAuthOptions);
