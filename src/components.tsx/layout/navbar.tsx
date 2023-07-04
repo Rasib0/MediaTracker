82d18542777c6eddc31c39af01937e9a0ed90294
Rasib0 <rasibnadeem101@gmail.com>
2023-07-04 11:59:36 +0500

    removed testing red border

--- src/components.tsx/layout/navbar.tsx
+++ src/components.tsx/layout/navbar.tsx
@@ -9,7 +9,6 @@ type Props = {
   currentPage: currentPage;
 };
 
-
 // TODO make menu state stick using useLocalStorage hook
 export const Navbar = (props: Props) => {
   const [userButtonClicked, setUserButtonClicked] = useState(false);
@@ -148,7 +147,7 @@ export const Navbar = (props: Props) => {
             </div>
           </div>
 
-          <div className="absolute inset-y-0 right-0 flex items-center border border-red-700 pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
               type="button"
               className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
