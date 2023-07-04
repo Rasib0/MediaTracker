41072c7226eebc309f3e6d537351485c807ad18d
Rasib0 <rasibnadeem101@gmail.com>
2023-07-04 11:00:27 +0500

    fixed layout

--- src/components.tsx/card.tsx
+++ src/components.tsx/card.tsx
@@ -24,7 +24,7 @@ export type CardProps = {
 const Card = (props: CardProps) => {
   return (
     <Link
-      className="m-2 min-w-fit transform rounded border shadow-lg transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 hover:shadow-xl"
+      className="m-2 min-w-fit transform overflow-hidden overflow-ellipsis rounded border shadow-lg transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-105 hover:shadow-xl"
       href={"/" + props.type.slice(0, -1) + "/" + props.media_url}
     >
       <div className="flex justify-center font-semibold">
@@ -32,24 +32,26 @@ const Card = (props: CardProps) => {
         &nbsp;
         {props.show_author && <div>by {props.by}</div>}
       </div>
-      <div className="flex justify-between border border-black from-purple-600 to-purple-700 px-2 py-1 dark:bg-gradient-to-r">
-        {props.rating && <RatingStatic rating={props.rating} />}
+      {(props.rating || props.date) && (
+        <div className="flex justify-between border border-black from-purple-600 to-purple-700 px-2 py-1 dark:bg-gradient-to-r">
+          {props.rating && <RatingStatic rating={props.rating} />}
+          {props.date && (
+            <p className="font-thin">Added {dayjs(props.date).fromNow()}</p>
+          )}
+        </div>
+      )}
 
-        {props.date && (
-          <p className="font-thin">Added {dayjs(props.date).fromNow()}</p>
-        )}
-      </div>
       <div className="flex gap-2 border-t p-2">
         <Image
           src={"/images/" + props.type + "/" + props.image_url + ".jpg"}
           className="rounded "
           width={130}
-          height={200}
+          height={210}
           alt="..."
         />
-        <div className="max-h-56 overflow-x-hidden overflow-y-scroll overflow-ellipsis p-2">
+        <p className="max-h-56 overflow-clip overflow-ellipsis p-2">
           {props.synopsis}
-        </div>
+        </p>
       </div>
     </Link>
   );
