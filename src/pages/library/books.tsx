cb978d57c9d6668f13bfd8e611bfd994bfa9dd2e
Rasib0 <rasibnadeem101@gmail.com>
2023-07-04 10:01:16 +0500

    fixed library

--- src/pages/library/books.tsx
+++ src/pages/library/books.tsx
@@ -6,14 +6,15 @@ import Card from "../../components.tsx/card";
 import Layout from "../../components.tsx/layout";
 import { currentPage } from "~/common/types";
 import { LoadingSpinner } from "~/components.tsx/loading";
+import Link from "next/link";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const getServerSideProps = requireAuth(async (_ctx) => {
   return { props: {} };
 });
-
+// remember that author and director are the same thing so you can replace
 // The page that you only see if the authentication is successful, we could revamp this page to only should non-sensistive information still the login occurs if we used
-const Dashboard: NextPage = () => {
+const LibraryBooks: NextPage = () => {
   const { data: sessionData } = useSession();
 
   const {
@@ -49,65 +50,87 @@ const Dashboard: NextPage = () => {
       <div className="flex flex-col items-center p-2">
         <h1 className="text-2xl font-bold">
           <span className="font-mono">{sessionData.user.username}</span>&apos;s
-          Library Page
+          book library
         </h1>
         <p>Here is your library where you can see your book collection!</p>
       </div>
 
       <h2 className="p-2 text-lg font-semibold">Recently Added</h2>
       <hr className="mb-2" />
-      {(isLoadingRecent || isFetchingRecent) && (
-        <div className="flex justify-center p-24">
-          <LoadingSpinner />
-        </div>
-      )}
-      {queryDataRecent && queryDataRecent.result.length !== 0 ? (
-        <RecentGrid queryData={queryDataRecent} />
-      ) : (
-        <div>No books found 🤖</div>
-      )}
-
+      <RecentFeed
+        isLoadingRecent={isLoadingRecent}
+        isFetchingRecent={isFetchingRecent}
+        queryDataFav={{ queryData: queryDataRecent }}
+      />
       <h2 className="mt-4 p-2 text-lg font-semibold">Favorites</h2>
 
       <hr className="mb-2" />
-      {(isLoadingFav || isFetchingFav) && (
-        <div className="flex justify-center p-24">
-          <LoadingSpinner />
-        </div>
-      )}
-      {queryDataFav && queryDataFav.result.length !== 0 ? (
-        <FavGrid queryData={queryDataFav} />
-      ) : (
-        <div>Not a single book found in your favourites 😿</div>
-      )}
+      <FavFeed
+        isLoadingFav={isLoadingFav}
+        isFetchingFav={isFetchingFav}
+        queryDataFav={{ queryData: queryDataFav }}
+      />
     </Layout>
   );
 };
 
-export default Dashboard;
-
-type Props = {
-  queryData: {
-    message: string;
-    result: {
-      book: {
-        name: string;
-        image_url: string;
-        synopsis: string;
-        author: string;
-        book_url: string;
-      };
-      assignedAt: string;
-      Rating: number;
-      bookId: number;
-    }[];
+type FavFeedProps = {
+  queryDataFav: {
+    queryData:
+      | {
+          message: string;
+          result: {
+            book: {
+              name: string;
+              image_url: string;
+              synopsis: string;
+              author: string;
+              book_url: string;
+            };
+            assignedAt: string;
+            Rating: number;
+            bookId: number;
+          }[];
+        }
+      | undefined;
   };
+  isLoadingFav: boolean;
+  isFetchingFav: boolean;
 };
 
-const FavGrid = (props: Props) => {
+const FavFeed = (props: FavFeedProps) => {
+  if (props.isLoadingFav || props.isFetchingFav) {
+    return (
+      <div className="flex justify-center p-24">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (
+    !props.queryDataFav.queryData ||
+    props.queryDataFav.queryData.result.length === 0
+  ) {
+    return (
+      <div className="flex flex-col items-center p-24">
+        <div>Not a single book found in your favourites 😿</div>
+        <div>
+          Click{" "}
+          <Link
+            href="/book/all"
+            className="hover:underline dark:text-violet-400"
+          >
+            here
+          </Link>{" "}
+          to view our collection instead.
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {props.queryData.result.map((element, i) => {
+      {props.queryDataFav.queryData.result.map((element, i) => {
         return (
           <Card
             key={i}
@@ -127,18 +150,71 @@ const FavGrid = (props: Props) => {
   );
 };
 
-const RecentGrid = (props: Props) => {
+type RecentFeedProps = {
+  queryDataFav: {
+    queryData:
+      | {
+          message: string;
+          result: {
+            book: {
+              name: string;
+              image_url: string;
+              synopsis: string;
+              author: string;
+              book_url: string;
+            };
+            assignedAt: string;
+            Rating: number;
+            bookId: number;
+          }[];
+        }
+      | undefined;
+  };
+  isLoadingRecent: boolean;
+  isFetchingRecent: boolean;
+};
+
+const RecentFeed = (props: RecentFeedProps) => {
+  if (props.isLoadingRecent || props.isFetchingRecent) {
+    return (
+      <div className="flex justify-center p-24">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (
+    !props.queryDataFav.queryData ||
+    props.queryDataFav.queryData.result.length === 0
+  ) {
+    return (
+      <div className="flex justify-center p-24">
+        <div>No books found 🤖</div>
+        <div>
+          Click{" "}
+          <Link
+            href="/book/all"
+            className="hover:underline dark:text-violet-400"
+          >
+            here
+          </Link>{" "}
+          to view our collection instead.
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {props.queryData.result.map((element, i) => {
+      {props.queryDataFav.queryData.result.map((element, i) => {
         return (
           <Card
             key={i}
             name={element.book.name}
             type="books"
+            show_author={false}
             rating={element.Rating}
             by={element.book.author}
-            show_author={false}
             synopsis={element.book.synopsis}
             date={new Date(element.assignedAt)}
             image_url={element.book.image_url}
@@ -149,3 +225,5 @@ const RecentGrid = (props: Props) => {
     </div>
   );
 };
+
+export default LibraryBooks;
--- src/pages/library/movies.tsx
+++ src/pages/library/movies.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
-import router from "next/router";
 import { requireAuth } from "../../common/requireAuth";
 import { trpc } from "../../common/trpc";
 import Card from "../../components.tsx/card";
 import Layout from "../../components.tsx/layout";
 import { currentPage } from "~/common/types";
+import { LoadingSpinner } from "~/components.tsx/loading";
+import Link from "next/link";
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const getServerSideProps = requireAuth(async (_ctx) => {
@@ -13,101 +14,216 @@ export const getServerSideProps = requireAuth(async (_ctx) => {
 });
 
 // The page that you only see if the authentication is successful, we could revamp this page to only should non-sensistive information still the login occurs if we used
-const Dashboard: NextPage = () => {
-  const { data } = useSession();
+const LibraryMovies: NextPage = () => {
+  const { data: sessionData } = useSession();
 
-  const AllMovieInLibrarySortedRecent =
-    trpc.AllMovieInLibrarySortedRecent.useQuery({ keyword: "", take: 5, data });
-  const AllMovieInLibrarySortedRecentFav =
-    trpc.AllMovieInLibrarySortedRecentFav.useQuery({
-      keyword: "",
-      take: 5,
-      data,
-    });
+  const {
+    data: queryDataRecent,
+    isLoading: isLoadingRecent,
+    isFetching: isFetchingRecent,
+  } = trpc.AllMovieInLibrarySortedRecent.useQuery({
+    keyword: "",
+    take: 5,
+    data: sessionData,
+  });
+
+  const {
+    data: queryDataFav,
+    isLoading: isLoadingFav,
+    isFetching: isFetchingFav,
+  } = trpc.AllMovieInLibrarySortedRecentFav.useQuery({
+    keyword: "",
+    take: 5,
+    data: sessionData,
+  });
+
+  if (!sessionData) {
+    return (
+      <div className="flex justify-center p-24">
+        <LoadingSpinner />
+      </div>
+    );
+  }
 
   return (
     <Layout currentPage={currentPage.library}>
-      <div className="">
-        <div className="">
-          <div className="">
-            <div
-              className="bg-primary mb-2 cursor-pointer p-3"
-              onClick={() => {
-                void router.push("/library/movies/");
-              }}
-            >
-              <h1>{data?.user.username}&apos;s Movie Library Page</h1>Here is
-              your Movie library where you can see your Movie collection!
-            </div>
-            <div
-              className="bg-secondary mb-2 cursor-pointer p-3 text-white "
-              onClick={() => {
-                void router.push("/library/movies/recent");
-              }}
-            >
-              <h3>Recently Added</h3>
-            </div>
-
-            <div className="cards">
-              {AllMovieInLibrarySortedRecent.data?.result.map((input, i) => {
-                return (
-                  <Card
-                    key={i}
-                    name={input.movie.name}
-                    type="movies"
-                    rating={input.Rating}
-                    by={input.movie.director}
-                    synopsis={input.movie.synopsis}
-                    show_author={false}
-                    date={input.assignedAt}
-                    image_url={input.movie.image_url}
-                    media_url={input.movie.movie_url}
-                  />
-                );
-              })}
-            </div>
-            <div
-              className="bg-secondary mb-2 cursor-pointer p-3 text-white"
-              onClick={() => {
-                void router.push("/library/movies/favorites");
-              }}
-            >
-              <h3>Favorites</h3>
-            </div>
-            <div className="cards">
-              {AllMovieInLibrarySortedRecentFav.data?.result.map((input, i) => {
-                return (
-                  <Card
-                    key={i}
-                    name={input.movie.name}
-                    type={"movies"}
-                    rating={input.Rating}
-                    show_author={false}
-                    by={input.movie.director}
-                    synopsis={input.movie.synopsis}
-                    date={input.assignedAt}
-                    image_url={input.movie.image_url}
-                    media_url={input.movie.movie_url}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        </div>
+      <div className="flex flex-col items-center p-2">
+        <h1 className="text-2xl font-bold">
+          <span className="font-mono">{sessionData.user.username}</span>&apos;s
+          movie library
+        </h1>
+        <p>Here is your library where you can see your movie collection!</p>
       </div>
-      <style jsx>
-        {`
-          .cards {
-            display: grid;
-            grid-template-columns: repeat(
-              auto-fill,
-              minmax(min(399px, 100%), 1fr)
-            );
-          }
-        `}
-      </style>
+
+      <h2 className="p-2 text-lg font-semibold">Recently Added</h2>
+      <hr className="mb-2" />
+      <RecentFeed
+        isLoadingRecent={isLoadingRecent}
+        isFetchingRecent={isFetchingRecent}
+        queryDataFav={{ queryData: queryDataRecent }}
+      />
+      <h2 className="mt-4 p-2 text-lg font-semibold">Favorites</h2>
+
+      <hr className="mb-2" />
+      <FavFeed
+        isLoadingFav={isLoadingFav}
+        isFetchingFav={isFetchingFav}
+        queryDataFav={{ queryData: queryDataFav }}
+      />
     </Layout>
   );
 };
 
-export default Dashboard;
+type FavFeedProps = {
+  queryDataFav: {
+    queryData:
+      | {
+          message: string;
+          result: {
+            movie: {
+              name: string;
+              image_url: string;
+              synopsis: string;
+              director: string;
+              movie_url: string;
+            };
+            assignedAt: string;
+            Rating: number;
+            movieId: number;
+          }[];
+        }
+      | undefined;
+  };
+  isLoadingFav: boolean;
+  isFetchingFav: boolean;
+};
+
+const FavFeed = (props: FavFeedProps) => {
+  if (props.isLoadingFav || props.isFetchingFav) {
+    return (
+      <div className="flex justify-center p-24">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (
+    !props.queryDataFav.queryData ||
+    props.queryDataFav.queryData.result.length === 0
+  ) {
+    return (
+      <div className="flex flex-col items-center p-24">
+        <div>Not a single movie found in your favourites 😿</div>
+        <div>
+          Click{" "}
+          <Link
+            href="/movie/all"
+            className="hover:underline dark:text-violet-400"
+          >
+            here
+          </Link>{" "}
+          to view our collection instead.
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {props.queryDataFav.queryData.result.map((element, i) => {
+        return (
+          <Card
+            key={i}
+            name={element.movie.name}
+            type="movies"
+            show_author={false}
+            rating={element.Rating}
+            by={element.movie.director}
+            synopsis={element.movie.synopsis}
+            date={new Date(element.assignedAt)}
+            image_url={element.movie.image_url}
+            media_url={element.movie.movie_url}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+type RecentFeedProps = {
+  queryDataFav: {
+    queryData:
+      | {
+          message: string;
+          result: {
+            movie: {
+              name: string;
+              image_url: string;
+              synopsis: string;
+              director: string;
+              movie_url: string;
+            };
+            assignedAt: string;
+            Rating: number;
+            movieId: number;
+          }[];
+        }
+      | undefined;
+  };
+  isLoadingRecent: boolean;
+  isFetchingRecent: boolean;
+};
+
+const RecentFeed = (props: RecentFeedProps) => {
+  if (props.isLoadingRecent || props.isFetchingRecent) {
+    return (
+      <div className="flex justify-center p-24">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (
+    !props.queryDataFav.queryData ||
+    props.queryDataFav.queryData.result.length === 0
+  ) {
+    return (
+      <div className="flex justify-center p-24">
+        <div>No movies found 🤖</div>
+        <div>
+          Click{" "}
+          <Link
+            href="/movies/all"
+            className="hover:underline dark:text-violet-400"
+          >
+            here
+          </Link>{" "}
+          to view our collection instead.
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      {props.queryDataFav.queryData.result.map((element, i) => {
+        return (
+          <Card
+            key={i}
+            name={element.movie.name}
+            type="movies"
+            show_author={false}
+            rating={element.Rating}
+            by={element.movie.director}
+            synopsis={element.movie.synopsis}
+            date={new Date(element.assignedAt)}
+            image_url={element.movie.image_url}
+            media_url={element.movie.movie_url}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default LibraryMovies;
