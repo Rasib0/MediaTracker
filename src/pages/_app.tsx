e854fd86d6ec4b1f5b5d54e6c1595eae10e66e83
rasib <rasibnadeem101@gmail.com>
2023-07-05 00:08:57 +0500

    new favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { trpc } from "../common/trpc";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({
   Component,
@@ -12,11 +13,22 @@ function MyApp({
   session: Session;
 }>) {
   return (
-    <ThemeProvider attribute="class">
-      <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
-      </SessionProvider>
-    </ThemeProvider>
+    <>
+      <Head>
+        <style>
+          @import
+          url(&apos;https://fonts.googleapis.com/css2?family=Poppins&display=swap&apos;);
+        </style>
+        <title>MediaTracker</title>
+        <meta name="description" content="💬" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <ThemeProvider attribute="class">
+        <SessionProvider session={pageProps.session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
